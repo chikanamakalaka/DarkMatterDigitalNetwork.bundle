475b121383e48c24016a568dfe9948a6f5176e8d
Matt Hocker <mhocker@gmail.com>
2015-09-30 22:43:52 -0700

    Reorg

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,132 +1,50 @@
-ART      = 'art-default.jpg'
-ICON     = 'icon-default.png'
-
-PLAYLIST_URL_AAC = 'http://live.darkmatterdigitalnetwork.com/darkmatter.aac.m3u'
-PLAYLIST_URL_MP3 = 'http://live.darkmatterdigitalnetwork.com/darkmatter.mp3.m3u'
-METADATA_URL = 'http://radioparadise.com/ajax_xml_song_info.php?song_id=now'
-
-PREFIX = '/music/DarkMatterDigitalNetwork'
+ART = 'art-default.jpg'
+ICON = 'icon-default.jpg'
+NAME = 'Dark Matter Digital Network'
+STREAM_URL = 'http://live.darkmatterdigitalnetwork.com/darkmatter.mp3'
 
 ####################################################################################################
-
-# This function is initially called by the PMS framework to initialize the plugin. This includes
-# setting up the Plugin static instance along with the displayed artwork.
 def Start():
-    # Initialize the plugin
-    Plugin.AddViewGroup('List', viewMode = 'List', mediaType = 'items')
-    Plugin.AddViewGroup('InfoList', viewMode = 'InfoList', mediaType = 'items')
 
-    # Setup the artwork associated with the plugin
-    ObjectContainer.title1 = 'Dark Matter Digital Network'
-    ObjectContainer.art = R(ART)
-    ObjectContainer.view_group = 'List'
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	TrackObject.thumb = R(ICON)
 
-    DirectoryObject.thumb = R(ICON)
-
-    TrackObject.thumb = R(ICON)
-    TrackObject.art = R(ART)
-
-####################################################################################################
-
-@handler(PREFIX, 'Dark Matter Digital Network')
+####################################################################################################     
+@handler('/music/fuzionradio', NAME, thumb=ICON, art=ART)
 def MainMenu():
-    oc = ObjectContainer(no_cache = True)
 
-    # Would love to call this periodically to update the channel background art, but not possible
-    # oc.art = HTTP.Request('http://radioparadise.com/readtxt.php').content
+	oc = ObjectContainer()
+	oc.add(CreateTrackObject(url=STREAM_URL, title=NAME))
 
-    # Fetch XML with metadata and build XML Object
-    #song_info = XML.ObjectFromURL(METADATA_URL)
-
-    # Fetch AAC and MP3 urls
-    aac_url = HTTP.Request(PLAYLIST_URL_AAC).content.splitlines()[0]
-    mp3_url = HTTP.Request(PLAYLIST_URL_MP3).content.splitlines()[0]
-    
-    # Add playable TrackObject to menu structure
-    oc.add(
-        CreateTrackObject(
-            title = song_info.title.text,
-            artist = song_info.artist.text,
-            album = song_info.album.text,
-            rating = float(song_info.rating.text),
-            thumb = song_info.large_cover.text,
-            art = song_info.image.large_url.text,
-            aac_url = aac_url,
-            mp3_url = mp3_url
-        )
-    )
-
-    return oc
+	return oc
 
 ####################################################################################################
-@route(PREFIX + '/CreateTrackObject', rating = float, include_container = bool) 
-def CreateTrackObject(title, artist, album, rating, thumb, art, aac_url, mp3_url, include_container=False):        
-    track_object = TrackObject(
-        key = 
-            Callback(
-                CreateTrackObject,
-                title = title,
-                artist = artist,
-                album = album,
-                rating = rating,
-                thumb = thumb,
-                art = art,
-                aac_url = aac_url,
-                mp3_url = mp3_url,
-                include_container = True
-            ),
-        rating_key = title,
-        title = title,
-        artist = artist,
-        album = album,
-        rating = rating,
-        thumb = thumb,
-        art = art
-    )
-    
-    track_object.add(
-        MediaObject(
-            container = Container.MP3,
-            audio_codec = AudioCodec.MP3,
-            audio_channels = 2,
-            bitrate = 48,
-            parts = [
-                PartObject(
-                    key = Callback(PlayMP3, url = mp3_url)
-                )
-            ]
-        )
-    )
-    
-    track_object.add(
-        MediaObject(
-            container = Container.MP4,
-            audio_codec = AudioCodec.AAC,
-            audio_channels = 2,
-            bitrate = 48,
-            parts = [
-                PartObject(
-                    key = Callback(PlayAAC, url = aac_url)
-                )
-            ]
-        )
-    )
+def CreateTrackObject(url, title, include_container=False):
 
-    if include_container:
-        return ObjectContainer(objects=[track_object])
-    else:
-        return track_object
-        
-#################################################################################################### 
-@route(PREFIX + '/PlayMP3.mp3')
-def PlayMP3(url):
-    return PlayAudio(url)
-    
-#################################################################################################### 
-@route(PREFIX + '/PlayAAC.aac')
-def PlayAAC(url):
-    return PlayAudio(url)
-    
-#################################################################################################### 
+	track_object = TrackObject(
+		key = Callback(CreateTrackObject, url=url, title=title, include_container=True),
+		rating_key = url,
+		title = title,
+		items = [
+			MediaObject(
+				parts = [
+					PartObject(key=Callback(PlayAudio, url=url, ext='mp3'))
+				],
+				container = Container.MP3,
+				bitrate = 48,
+				audio_codec = AudioCodec.MP3,
+				audio_channels = 2
+			)
+		]
+	)
+
+	if include_container:
+		return ObjectContainer(objects=[track_object])
+	else:
+		return track_object
+
+####################################################################################################
 def PlayAudio(url):
-    return Redirect(url)
+
+	return Redirect(url)
